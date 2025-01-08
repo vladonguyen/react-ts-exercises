9d11ef5157e31ae7cad4a98dc58f04dc409c907b
Vladimir Nguyen <95983775+vladonguyen@users.noreply.github.com>
2025-01-08 23:52:14 +0200

    working with ref, useRef and function props

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import Todos from './components/Todos';
 import './App.css';
 import Todo from './models/todo';
+import NewTodo from './components/NewTodo';
 
 function App() {
   const todos = [
     new Todo('Learn React'),
     new Todo('Learn TS')
   ];
+
+  const addTodoHandler = (todoText: string)=> {
+
+  };
   return (
     <div>
+      <NewTodo onAddtodo={addTodoHandler}/>
     <Todos items={todos} />
     </div>
   );
---
+++ src/components/NewTodo.tsx
@@ -0,0 +1,24 @@
+import { useRef } from "react";
+
+const NewTodo: React.FC<{onAddtodo: (text: string)=> void }>= (props)=> {
+    const todoTextInputRef = useRef<HTMLInputElement>(null);
+    const submitHandler = (event: React.FormEvent)=> {
+        event.preventDefault();
+
+        const enteredText = todoTextInputRef.current!.value;
+
+        if(enteredText?.trim(). length === 0){
+            //throw an error
+            return;
+        }
+
+        props.onAddtodo(enteredText);
+    }
+return <form onSubmit={submitHandler}>
+    <label htmlFor="text">Todo text</label>
+    <input type="text" ref={todoTextInputRef}/>
+    <button>Add Todo</button>
+</form>
+}
+
+export default NewTodo;
